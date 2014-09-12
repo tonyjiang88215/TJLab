15369e1e731fbbe441c546e163b1e851bbf6c84c
zhengjiang <zhengjiang@chanjet.com>
2014-09-12 20:22:34 +0800

    udate

---
+++ chrome/pageshot/popup.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tony on 2014/9/12.
+ */
+
+document.getElementById('btn').addEventListener('click' , function(){
+	chrome.windows.getCurrent(function(window){
+		chrome.tabs.captureVisibleTab(window.id , {} , function(image){
+			alert(image);
+		});
+	});
+
+});
