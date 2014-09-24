c292d574a4f3df40c6b11b00c3111b5060e51009
tonyjiang88215 <12003454@qq.com>
2014-09-24 22:05:59 +0800

    1
    1

--- chrome/pageshot/run.js
+++ chrome/pageshot/run.js
@@ -3,3 +3,19 @@
  */
 //alert('i am running');
 
+//获取当前窗口对象
+chrome.windows.getCurrent(function(window){
+
+    console.log(window);
+
+});
+
+//chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+//    alert(tab.url);
+//})
+
+chrome.tabs.getSelected(function(w){
+
+    console.log(w);
+
+})
